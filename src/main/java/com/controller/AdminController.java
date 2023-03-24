e1274de55ad4f439f8f3b55d33b5434bc7b9da9a
freeeeezin <jjjkk5857@gmail.com>
2023-03-24 14:24:56 +0900

    admin

--- src/main/java/com/controller/AdminController.java
+++ src/main/java/com/controller/AdminController.java
@@ -1,11 +1,15 @@
 package com.controller;
 
 import com.model.User;
+import com.model.kream.cs.Notice;
+import com.model.kream.cs.Qna;
 import com.model.kream.home.Banner;
 import com.model.kream.point.Point;
 import com.response.DefaultRes;
 import com.response.Message;
 import com.service.BannerService;
+import com.service.NoticeService;
+import com.service.QnaService;
 import com.service.kream.user.PointService;
 import com.service.kream.user.UserService;
 import lombok.RequiredArgsConstructor;
@@ -26,6 +30,8 @@ public class AdminController {
     private final BannerService bannerService;
     private final UserService userService;
     private final PointService pointService;
+    private final NoticeService noticeService;
+    private final QnaService qnaService;
 
     @GetMapping("/banner")
     public ModelAndView getBanner(){
@@ -50,6 +56,31 @@ public class AdminController {
 
         return  view;
     }
+
+    @GetMapping("/notice")
+    public ModelAndView getNotice(@PathVariable int page){
+        Message message = new Message();
+        ModelAndView view = new ModelAndView("/admin");
+
+        List<Notice> notices = noticeService.getNotices(page);
+        view.addObject("notices",notices);
+
+        return  view;
+    }
+
+    @GetMapping("/qna")
+    public ModelAndView getNotice(){
+        Message message = new Message();
+        ModelAndView view = new ModelAndView("/admin");
+
+        List<Qna> qnas = qnaService.getAllQna();
+        view.addObject("qnas",qnas);
+
+        return  view;
+    }
+
+
+
 
 
 
